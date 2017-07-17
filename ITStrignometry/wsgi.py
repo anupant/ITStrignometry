7371b8979144c4de84a96174b70ca9534d79f1b1
Anu Pant <apant7@gatech.edu>
2017-07-16 17:32:40 -0700

    2nd commit

--- ITStrignometry/wsgi.py
+++ ITStrignometry/wsgi.py
@@ -5,7 +5,8 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
-"""
+
+
 
 import os
 
@@ -14,3 +15,21 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ITStrignometry.settings")
 
 application = get_wsgi_application()
+"""
+
+import os
+import sys
+
+
+#path = '/home/pythonanywhereusername/projectname' #Use your own pythonanywhere username
+path = '/home/curiouslearner/ITStrignometry'
+
+if path not in sys.path:
+    sys.path.append(path)
+
+#os.environ['DJANGO_SETTINGS_MODULE'] = 'projectname.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'ITStrignometry.settings'
+
+from django.core.wsgi import get_wsgi_application
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+application = StaticFilesHandler(get_wsgi_application())
